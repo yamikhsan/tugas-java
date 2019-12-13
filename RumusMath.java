83b9dfe02745a50da2da151c3ed63c914550eacb
yamikhsan <yahyamaulana450@gmail.com>
2019-12-13 14:27:35 +0700

    v1

--- RumusMath.java
+++ RumusMath.java
@@ -13,16 +13,162 @@ public class RumusMath{
     }
 
     private static void menu(){
-        println("Menu");
-        println("1. Luas Segitiga");
-        println("2. Volume Balok");
-        println("3. Volume Prisma Segitiga");
-        println("4. Volume Kerucut");
-        println("5. Exit");
+            
+            boolean run = true;
+    
+            while(run){
 
-        print("Pilih menu = ");
+                try {
+    
+                    println("------------------------------------------------");
+        
+                    println("Menu");
+                    println("1. Luas Segitiga");
+                    println("2. Volume Balok");
+                    println("3. Volume Prisma Segitiga");
+                    println("4. Volume Kerucut");
+                    println("5. Exit");
+            
+                    print("Pilih menu = ");
+        
+                    int i = scan.nextInt();
+        
+                    switch (i) {
+                        case 1:
+                            luasSegitiga();
+                            exitMenu();
+                            break;
+        
+                        case 2:
+                            volumeBalok();
+                            exitMenu();
+                            break;
+        
+                        case 3:
+                            volumePrismaSegitiga();
+                            exitMenu();
+                            break;
+        
+                        case 4:
+                            volumeKerucut();
+                            exitMenu();
+                            break;
+        
+                        case 5:
+                            run = false; 
+                            break;
+                    
+                        default:
+                            println("Menu tidak ada");
+                            break;
+                    }
+        
+                    println("------------------------------------------------");
+
+                } catch (Exception e) {
+                    println("harus input type data int");
+                    run = false;
+                }
+    
+            } 
+        
+    }
+
+    private static void luasSegitiga() {
+
+        try {
+            
+            print("Alas = ");
+            double a = scan.nextDouble();
+
+            print("Tinggi = ");
+            double t = scan.nextDouble();
+    
+            double hasil = segitiga(a, t);
+            println("Luas = " + hasil);
+
+        } catch (Exception e) {
+            println("Inputan data harus tipe double");
+        }
+        
+    }
+
+    private static void volumeBalok() {
+
+        try {
+            
+            print("Panjang = ");
+            double p = scan.nextDouble();
+
+            print("Lebar = ");
+            double l = scan.nextDouble();
+
+            print("Tinggi = ");
+            double t = scan.nextDouble();
+    
+            double hasil = p * l * t;
+            println("Volume = " + hasil);
+
+        } catch (Exception e) {
+            println("Inputan data harus tipe double");
+        }
+        
+    }
+
+    private static void volumePrismaSegitiga() {
+
+        try {
+            
+            print("Alas = ");
+            double a = scan.nextDouble();
+
+            print("Lebar = ");
+            double l = scan.nextDouble();
+
+            print("Tinggi = ");
+            double t = scan.nextDouble();
+    
+            double segitiga = segitiga(a, l);
+            double hasil =  segitiga * t ;
+            println("Volume = " + hasil);
+
+        } catch (Exception e) {
+            println("Inputan data harus tipe double");
+        }
+
+    }
+
+    private static void volumeKerucut() {
+
+        final double pi = 3.14;
+        
+        try {
+            
+            print("Jari-jari = ");
+            double r = scan.nextDouble();
+
+            print("Tinggi = ");
+            double t = scan.nextDouble();
+
+            double hasil = (pi * r * r) * t / 3;
+            println("Volume = " + hasil);
+
+        } catch (Exception e) {
+            println("Inputan data harus tipe double");
+        }
+        
+    }
+
+    private static void exitMenu(){
+        print("Input 1 jika ingin lanjut jika tidak input apa saja = ");
         int res = scan.nextInt();
-        println(res);
+        if(res != 1){
+            System.exit(1);
+        }
+    }
+
+    private static double segitiga(double a, double t){
+        return a * t / 2;
     }
 
     private static void println(Object obj){
